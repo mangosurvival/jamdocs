ea3bc7f7931c2f35b65f2a1fd0555a040f474e74
mangosurvival <pedraja.andres7@gmail.com>
2022-09-03 17:14:38 -0400

    a

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,7 +16,7 @@ function addStyleResource (rule) {
 }
 
 module.exports = {
-  siteName: 'Jamdocs',
+  siteName: 'MangoSV',
   siteUrl: 'https://jamdocs.samuelhorn.com',
   templates: {
     Doc: '/:slug',
