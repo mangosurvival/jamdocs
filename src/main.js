162607cd1196a090a74e3c537daed2e22a349e77
mangosurvival <pedraja.andres7@gmail.com>
2022-09-03 16:48:13 -0400

    Revert "a"
    This reverts commit 92555415860dcd442097958bdda9b09acde082a2.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import '~/assets/scss/globals.scss'
 import Vuex from 'vuex'
-require('typeface-open-sans')
+require('typeface-source-sans-pro')
 
 export default function (Vue, { router, head, isClient, appOptions }) {
   Vue.use(Vuex)
